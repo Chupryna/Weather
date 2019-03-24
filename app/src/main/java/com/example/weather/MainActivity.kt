abece978ea69883ab8e5eb8e5f17af4cb1c405f3
Tolik Chupryna <chupryna.lviv@i.ua>
2019-03-24 14:21:40 +0200

    added hide keyboard and progress bar

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -6,23 +6,26 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.example.weather.data.model.ListItem
 import com.example.weather.data.source.DataSource
 import com.example.weather.data.source.WeatherDataSource
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnGetWeather.setOnClickListener(this)
+        btnGetWeather.setOnClickListener{ onLoadWeather(it) }
         recyclerShowWeather.layoutManager = LinearLayoutManager(this)
     }
 
-    override fun onClick(v: View?) {
+    private fun onLoadWeather(v: View) {
+        hideKeyboard(v)
+        showProgress(true)
         val city = editCity.text.toString()
 
         if(!isNetworkAvailable()) {
@@ -34,11 +37,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //weatherDataSource.getWeatherByID(524901)
         weatherDataSource.getWeatherByName(city, object: DataSource.LoadWeatherCallBack{
             override fun onWeatherLoaded(list: List<ListItem>) {
+                showProgress(false)
                 val adapter = RVAdapterWeather(list)
                 recyclerShowWeather.adapter = adapter
             }
 
             override fun onFailure() {
+                showProgress(false)
                 Toast.makeText(this@MainActivity, R.string.failed_load_weather, Toast.LENGTH_SHORT).show()
             }
         })
@@ -51,5 +56,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             return true
 
         return false
+    }
+
+    private fun hideKeyboard(view: View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun showProgress(show: Boolean) {
+        if (show)
+            containerProgressBar.visibility = View.VISIBLE
+        else
+            containerProgressBar.visibility = View.GONE
     }
 }
